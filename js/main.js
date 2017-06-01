4a26c5c421feef28fcc4d98959872a8888d672a7
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2017-06-01 11:48:29 +0300

    Верхее меню сделал

---
+++ js/main.js
@@ -0,0 +1,36 @@
+  // Declare the variable
+  var toggles = $(".burger");
+
+// This function adds the class that displays the menu
+  toggles.on("click", function() {
+    $(this).closest(".menu").toggleClass("open");
+    $(this).toggleClass('burger--close');
+  });
+
+// Declare the variable
+  var toggleLink = $(".link");
+
+// This function removes the class that displays the menu
+  toggleLink.on("click", function() {
+    $(this).closest(".menu").toggleClass("open");
+    $('.burger').removeClass('burger--close');
+  });
+
+  $(".menu").on("click","a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+    var id  = $(this).attr('href'),
+
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({scrollTop: top}, 1500);
+
+  });
+
+
+
+
